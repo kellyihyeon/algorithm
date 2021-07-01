30c3923965a3453cfa7c27e3c046d8f57786bce0
kellykang-tech <kellykang13@gmail.com>
2021-07-01 09:55:21 +0900

    :memo: Add: 기초-입출력[완료] 1027.

--- basic-100/src/com/code/print/InputOutput16.java
+++ basic-100/src/com/code/print/InputOutput16.java
@@ -7,10 +7,14 @@ public class InputOutput16 {
     // 1027
     public static void main(String[] args) {
 
-//        Scanner sc = new Scanner(System.in);
-//
-//        String date = sc.next();
-//        date.replace(".", "-");
+        Scanner sc = new Scanner(System.in);
+        // 1. 입력 받기
+        String date = sc.next();
+
+        // 2. 년.월.일 분리하기 -> 배열에 년.월.일 저장하기
+        String[] dateSplit = date.split("\\.");
+
+        System.out.printf("%s-%s-%s", dateSplit[2], dateSplit[1], dateSplit[0]);
 
     } //main
 }
