47708418f0914f8abb180bbbc63786b9bb79d5e8
kellykang-tech <kellykang13@gmail.com>
2021-08-30 23:43:09 +0900

    :memo: Add: 1. String - 10 풀이

--- PreparingCodingTest/src/section01/section0110/Main.java
+++ PreparingCodingTest/src/section01/section0110/Main.java
@@ -3,8 +3,30 @@ package section01.section0110;
 import java.util.Scanner;
 
 class Main {
-    public String solution(String str) {
-        String answer = "";
+    public int[] solution(String s, char t) {
+        int[] answer = new int[s.length()];
+        int p = 1000;
+
+        // 왼쪽으로부터 떨어진 거리
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == t) {
+                p = 0;
+                answer[i] = p;
+            } else {
+                p++;
+                answer[i] = p;
+            }
+        }
+
+        p = 1000;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == t) {
+                p = 0;
+            } else {
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+        }
 
         return answer;
 
@@ -15,6 +37,10 @@ class Main {
         Scanner kb = new Scanner(System.in);
 
         String str = kb.next();
-        System.out.print(T.solution(str));
+        char c = kb.next().charAt(0);
+
+        for (int x : T.solution(str, c)) {
+            System.out.print(x + " ");
+        }
     }
 }
