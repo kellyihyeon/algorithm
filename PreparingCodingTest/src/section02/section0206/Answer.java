5ee46ae9edde7fd4dc4f68866f781ca026e7f04c
kellykang-tech <kellykang13@gmail.com>
2022-01-09 17:33:15 +0900

    :memo: Add: 뒤집은 소수 구하기 풀이

--- PreparingCodingTest/src/section02/section0206/Answer.java
+++ PreparingCodingTest/src/section02/section0206/Answer.java
@@ -1,8 +1,53 @@
 package section02.section0206;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Answer {
 
+    private ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        // n = 9, arr = [100, 34, 224, 63]
+        for (int i = 0; i < n; i++) {
+            int tmp = arr[i];
+            int res = 0;
+            while (tmp > 0) {
+                int t = tmp % 10;
+                res = res * 10 + t;
+                tmp = tmp / 10;
+            }
+            if (isPrime(res)) {
+                answer.add(res);
+            }
+
+        }
+        return answer;
+    }
+
+    private boolean isPrime(int num) {
+        if (num == 1) {
+            return false;
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        
+        Answer answer = new Answer();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        for (int x : answer.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
     }
 }
