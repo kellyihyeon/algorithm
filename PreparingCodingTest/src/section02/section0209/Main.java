3e9959762be785fd98600dc88af54d6f96cf2310
kellykang-tech <kellykang13@gmail.com>
2022-01-15 21:05:35 +0900

    :memo: Add: 0209 격자판 최대합 구하기 정답

--- PreparingCodingTest/src/section02/section0209/Main.java
+++ PreparingCodingTest/src/section02/section0209/Main.java
@@ -42,6 +42,34 @@ public class Main {
         return max;
     }
 
+    private static int answer(int n, int[][] arr) {
+        int answer = Integer.MIN_VALUE;
+        int sum1;
+        int sum2;
+
+        for (int i = 0; i < n; i++) {
+            sum1 = 0;
+            sum2 = 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+
+        sum1 = 0;
+        sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[i][n - i - 1];
+        }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -54,7 +82,7 @@ public class Main {
             }
         }
 
-        System.out.println(solution(n, arr));
+        System.out.println(answer(n, arr));
     }
 
 }
