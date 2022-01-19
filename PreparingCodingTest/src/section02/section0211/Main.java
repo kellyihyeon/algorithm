d5696150571b33836ffc310333cc78c8743766f3
kellykang-tech <kellykang13@gmail.com>
2022-01-19 21:43:18 +0900

    :memo: Add: 2011 임시반장 구하기문제 오답 해결

--- PreparingCodingTest/src/section02/section0211/Main.java
+++ PreparingCodingTest/src/section02/section0211/Main.java
@@ -5,40 +5,37 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- * 수정 중...
- * 정답 맞혀보자...
+ * list 를 안 비우고 계속 쌓아뒀었네 ㅠㅠ
  */
 public class Main {
 
     public int solution(int n, int[][] arr) {
-        int studentIndex = 0;
+        int studentIndex = 1;
         int[] result = new int[n];
         List<Integer> done = new ArrayList<>();
 
-        // n = 9;
         for (int i = 0; i < n; i++) {
             int matching = 0;
+            done.clear();   // clear !!
             for (int j = 0; j < 5; j++) {
                 for (int k = 0; k < n; k++) {
-                    if (i != k) {
-                        // 0,0 vs  0,0 (1,0)(2,0--)(3,0)(4,0)(5,0)(6,0)(7,0)(8,0)(9,0)
-                        // 0,1 vs  0,1 (1,1)(2,1)(3,1)(4,1)(5,1)(6,1)(7,1)(8,1)(9,1)
-                        if (arr[i][j] == arr[k][j] && !done.contains(k)) {
-                            done.add(k);    //[1, 4]
-                            matching++;
-                        }
+                    if (!done.contains(k) && arr[i][j] == arr[k][j]) {
+                        done.add(k);
+                        matching++;
                     }
+
                 }//for3-k
+
             }//for2-j
-            result[i] = matching;   // [0, 0, 0, ]
-            System.out.println(i + "번째 학생 = " + matching);
+            result[i] = matching;
         }//for1-i
+
 
         int max = result[0];
         for (int i = 1; i < result.length; i++) {
             if (max < result[i]) {
-                max = result[i];        // 2
-                studentIndex = i + 1;   // 3
+                max = result[i];
+                studentIndex = i + 1;
             }
         }
 
