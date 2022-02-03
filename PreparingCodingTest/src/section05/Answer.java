f47efbd535f88756ebeb03e3df27f33b97d114a8
kellykang-tech <kellykang13@gmail.com>
2022-02-03 12:54:07 +0900

    :memo: Add: 0501 올바른 괄호 - stack 해설코드

--- PreparingCodingTest/src/section05/Answer.java
+++ PreparingCodingTest/src/section05/Answer.java
@@ -1,6 +1,7 @@
 package section05;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * stack 으로 풀기
@@ -9,6 +10,21 @@ public class Answer {
 
     private String solution(String str) {
         String answer = "YES";
+        Stack<Character> stack = new Stack<>();
+        for (char x : str.toCharArray()) {
+            if (x == '(') {
+                stack.push(x);
+            } else {
+                if (stack.isEmpty()) {
+                    return "NO";    // 바로 return 해주니까 else 분기문을 적을 필요가 없네
+                }
+                stack.pop();
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            return "NO";
+        }
 
         return answer;
     }
@@ -18,7 +34,6 @@ public class Answer {
         Scanner scanner = new Scanner(System.in);
 
         String str = scanner.nextLine();
-
         System.out.println(M.solution(str));
 
     }
