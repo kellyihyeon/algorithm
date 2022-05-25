7aac2952a99956ec9ad8da7891901dff3e87e7c5
kellykang-tech <kellykang13@gmail.com>
2022-05-25 17:09:10 +0900

    refactor: BinarySearch, FirstBadVersion

--- LeetCode/src/com/lab/studyplan/algorithm1/binarysearch/FirstBadVersion278.java
+++ LeetCode/src/com/lab/studyplan/algorithm1/binarysearch/FirstBadVersion278.java
@@ -3,31 +3,51 @@ package com.lab.studyplan.algorithm1.binarysearch;
 public class FirstBadVersion278 {
 
     boolean isBadVersion(int version) {
-        // VersionControl 에 정의된 메서드
+        // first bad is 4
+        if (version < 4) {
+            return false;
+        }
         return true;
     }
 
     public int binary(int low, int high) {
-
-
         if(low < high) {
             int mid = low + (high - low)/2;
+
             if(isBadVersion(mid)) {
                 return binary(low, mid);
             } else {
-                return binary(mid+1, high);
+                return binary(mid + 1, high);
             }
         }
         return low;
     }
+
+    // 1. 재귀로 푸는 방식
     public int firstBadVersion(int n) {
 
         return binary(1, n);
     }
 
+    // 2.
+    public int firstBadVersion2(int n) {
+        int left = 1;
+        int right = n;
 
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
 
     public static void main(String[] args) {
-
+        final FirstBadVersion278 F = new FirstBadVersion278();
+        System.out.println(F.firstBadVersion(5));
     }
 }
