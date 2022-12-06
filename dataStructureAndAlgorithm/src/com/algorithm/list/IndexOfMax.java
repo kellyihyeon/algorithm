b034703ab0c55e49b1a0588ca66b229d60e51a83
kellykang-tech <kellykang13@gmail.com>
2022-12-06 23:13:36 +0900

    sol: 최대값 인덱스, 순열 검사

---
+++ dataStructureAndAlgorithm/src/com/algorithm/list/IndexOfMax.java
@@ -0,0 +1,32 @@
+package com.algorithm.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IndexOfMax {
+
+    // input = [1, 3, 5, 4, 5, 2, 1]
+    public List<Integer> solution(int[] arr) {
+        // 최대값이 배열 arr 안에 몇개가 있을지 모르니 list 사용
+        List<Integer> indexes = new ArrayList<>();
+
+        // 최대값 구하기 -> 최대값 지정 후 루프를 돌면서 arr 값과 max 를 비교하면서 max 설정
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        // 인덱스 저장하기
+        for (int i = 0; i < arr.length; i++) {
+            // max 와 같은 값이 있다면 list 자료구조에 인덱스를 저장
+            if (max == arr[i]) {
+                indexes.add(i);
+            }
+        }
+        // 리스트를 배열로 변환하기
+
+        return indexes;
+    }
+    
+}
