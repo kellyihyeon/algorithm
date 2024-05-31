d62dd778511bbc574c320a762be03399c887913c
kellyihyeon <kellykang13@gmail.com>
2024-05-31 10:53:01 +0900

    feat: Update 8-2 타겟넘버 풀이

--- dataStructureAndAlgorithm/src/com/algorithm/graphandnonlinearsearch/TargetNumber.java
+++ dataStructureAndAlgorithm/src/com/algorithm/graphandnonlinearsearch/TargetNumber.java
@@ -2,6 +2,8 @@ package com.algorithm.graphandnonlinearsearch;
 
 public class TargetNumber {
 
+    int answer;
+
     public static void main(String[] args) {
         int[] numbers = {1, 1, 1, 1, 1};
         int target = 3;
@@ -11,7 +13,23 @@ public class TargetNumber {
     }
 
     public int solution(int[] numbers, int target) {
-        return dfs(numbers, target, 0, 0);
+        return dfs2(numbers, target, 0, 0);
+    }
+
+    public int dfs2(final int[] numbers, final int target, int index, int sum) {
+        // 1. 탈출 조건
+        if (index == numbers.length) {
+            if (target == sum) {
+                answer++;
+            }
+            return answer;
+        }
+
+        // 2. 수행 동작
+        dfs2(numbers, target, index + 1, sum + numbers[index]);
+        dfs2(numbers, target, index + 1, sum - numbers[index]);
+
+        return answer;
     }
 
     public int dfs(final int[] numbers, final int target, int depth, int sum) {
@@ -24,6 +42,6 @@ public class TargetNumber {
         }
 
         return dfs(numbers, target, depth + 1, sum + numbers[depth]) +
-                dfs(numbers, target, depth + 1,sum - numbers[depth]);
+                dfs(numbers, target, depth + 1, sum - numbers[depth]);
     }
 }
