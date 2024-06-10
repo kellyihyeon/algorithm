5c94930babcadf011b289ba68e8c937fab09639c
kellyihyeon <kellykang13@gmail.com>
2024-06-10 12:33:25 +0900

    feat: Update 9-1 리팩토링 추가

--- dataStructureAndAlgorithm/src/com/algorithm/tree/ScovilleHeatUnit.java
+++ dataStructureAndAlgorithm/src/com/algorithm/tree/ScovilleHeatUnit.java
@@ -10,8 +10,31 @@ public class ScovilleHeatUnit {
         int k = 7;
         int[] scovilles = {1, 2, 3, 9, 10, 12};
 
-        int answer = new ScovilleHeatUnit().solution(scovilles, k);
+        int answer = new ScovilleHeatUnit().solution2(scovilles, k);
         System.out.println("answer = " + answer);
+    }
+
+    public int solution2(int[] scoville, int K) {
+        Queue<Integer> queue = new PriorityQueue<>();
+        for (int unit : scoville) {
+            queue.offer(unit);
+        }
+
+        int answer = 0;
+        while (queue.size() >= 2 && queue.peek() < K) {
+            Integer s1 = queue.poll();
+            Integer s2 = queue.poll();
+
+            int s3 = s1 + (s2 * 2);
+            queue.offer(s3);
+            answer++;
+        }
+
+        if (queue.peek() < K) {
+            return -1;
+        }
+
+        return answer;
     }
 
     public int solution(int[] scoville, int K) {
