b874444c1f0d1aa7059ab5c179681d96833ab6fc
kellyihyeon <kellykang13@gmail.com>
2024-08-10 00:31:55 +0900

    fix: 09-10026 메서드 분리

--- barkingdog-tutorial/src/com/github/kellyihyeon/section09bfs/CowArt.java
+++ barkingdog-tutorial/src/com/github/kellyihyeon/section09bfs/CowArt.java
@@ -3,6 +3,7 @@ package com.github.kellyihyeon.section09bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -11,12 +12,18 @@ import java.util.Queue;
  * 보통인이 봤을 때 그림 개수와 적록색약인 사람이 봤을 때의 그림 개수를 출력하라.
  */
 public class CowArt {
-    // 20 -> 00 : 18시 10분에 보통인 케이스1은 풀었고, 19분에 케이스2 풀이법 생각해냄.
+
+    private static int n;
+    private static char[][] board;
+    private static boolean[][] visited;
+    private final static int[] dirX = {1, 0, -1, 0};
+    private final static int[] dirY = {0, 1, 0, -1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        char[][] board = new char[n][n];
-        char[][] board2 = new char[n][n];
+        n = Integer.parseInt(br.readLine());
+        board = new char[n][n];
+        visited = new boolean[n][n];
 
         // 보통인의 그림 보드
         for (int i = 0; i < n; i++) {
@@ -25,89 +32,60 @@ public class CowArt {
                 board[i][j] = chars[j];
             }
         }
+        int normal = countImagePieces();
+
+        // 방문 초기화
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(visited[i], false);
+        }
 
         // 적록색약인의 그림 보드
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                char color;
                 if (board[i][j] == 'G') {
-                    color = 'R';
-                } else {
-                    color = board[i][j];
+                    board[i][j] = 'R';
                 }
-
-                board2[i][j] = color;
             }
         }
+        int redGreenColorBlindness = countImagePieces();
 
+        System.out.println(normal + " " + redGreenColorBlindness);
+    }
 
-        int[] dirX = {1, 0, -1, 0};
-        int[] dirY = {0, 1, 0, -1};
-
-        boolean[][] visited = new boolean[n][n];
-        Queue<int[]> queue = new LinkedList<>();
-
+    private static int countImagePieces() {
         int count = 0;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (visited[i][j]) continue;
-
-                visited[i][j] = true;
-                queue.offer(new int[]{i, j});
-
-                while (!queue.isEmpty()) {
-                    int[] cur = queue.poll();
-
-                    for (int dir = 0; dir < 4; dir++) {
-                        int nx = cur[0] + dirX[dir];
-                        int ny = cur[1] + dirY[dir];
-
-                        if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                        if (visited[nx][ny]) continue;
-                        if (board[cur[0]][cur[1]] != board[nx][ny]) continue;
-
-                        visited[nx][ny] = true;
-                        queue.offer(new int[]{nx, ny});
-                    }
-                }
-
+                bfs(i, j);
                 count++;
-
             }
         }
-
-        boolean[][] visited2 = new boolean[n][n];
-        Queue<int[]> queue2 = new LinkedList<>();
-
-        int count2 = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (visited2[i][j]) continue;
-
-                visited2[i][j] = true;
-                queue2.offer(new int[]{i, j});
-
-                while (!queue2.isEmpty()) {
-                    int[] cur = queue2.poll();
-
-                    for (int dir = 0; dir < 4; dir++) {
-                        int nx = cur[0] + dirX[dir];
-                        int ny = cur[1] + dirY[dir];
-
-                        if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                        if (visited2[nx][ny]) continue;
-                        if (board2[cur[0]][cur[1]] != board2[nx][ny]) continue;
-
-                        visited2[nx][ny] = true;
-                        queue2.offer(new int[]{nx, ny});
-                    }
-                }
-
-                count2++;
-            }
-        }
-
-        System.out.println(count + " " + count2);
+        return count;
     }
 
+    private static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+
+        visited[x][y] = true;
+        queue.offer(new int[]{x, y});
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = cur[0] + dirX[dir];
+                int ny = cur[1] + dirY[dir];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+                if (visited[nx][ny]) continue;
+                if (board[cur[0]][cur[1]] != board[nx][ny]) continue;
+
+                visited[nx][ny] = true;
+                queue.offer(new int[]{nx, ny});
+            }
+        }
+    }
 }
+
